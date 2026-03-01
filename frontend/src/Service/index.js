1674a3a5a2ab51785dd45ea5d0ed13856384f283
rupali dudhal <rupalidudhal95@gmail.com>
2026-03-01 21:20:36 +0530

    backend url added

--- frontend/src/Service/index.js
+++ frontend/src/Service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8082/tasks";
+const API_URL = "https://task-manager-gpv2.onrender.com/tasks";
 
 export const fetchTasks = async () => {
     const response = await axios.get(API_URL);
